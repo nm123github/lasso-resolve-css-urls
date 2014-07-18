834cca1d795424d024546b6e48f7338dca62e318
Phil Gates-Idem <phillip.idem@gmail.com>
2014-07-18 01:56:42 -0400

    Using callbacks when necessary (not promises)

--- raptor-optimizer-resolve-css-urls.js
+++ raptor-optimizer-resolve-css-urls.js
@@ -5,22 +5,28 @@ module.exports = {
     
     name: module.id,
 
+    // true: The transform function will RECEIVE and RETURN a stream that can be used to read the transformed out
+    // false: The transform function will RECEIVE full code and RETURN a value or promise
     stream: false,
 
-    transform: function(code, contentType, context) {
+    transform: function(code, contentType, optimizerContext) {
         if (contentType === 'css') {
 
-            var optimizer = context.optimizer;
-
-            var output = cssParser.replaceUrls(code, function(url) {
-                return optimizer.optimizeResource(url, context)
-                    .then(function(optimizedResource) {
-                        return optimizedResource.url;
-                    });
-            });
+            var optimizer = optimizerContext.optimizer;
 
             // NOTE: output could be either the String code or a promise, but we don't care
-            return output;
+            //       replaceUrls returns a promise
+            return cssParser.replaceUrls(code,
+
+                // the replacer function
+                function(url, start, end, callback) {
+                    optimizer.optimizeResource(url, optimizerContext, function(err, optimizedResource) {
+                        if (err) {
+                            // TODO: Log error?
+                        }
+                        callback(null, optimizedResource && optimizedResource.url);
+                    });
+                });
         }
         else {
             return code;
