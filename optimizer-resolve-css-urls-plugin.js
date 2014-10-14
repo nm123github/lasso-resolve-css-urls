98a1e5b712a1b819a85be809d3e707d009e28877
Phil Gates-Idem <phillip.idem@gmail.com>
2014-10-14 16:11:12 -0400

    Add support for custom URL resolver for CSS content

--- optimizer-resolve-css-urls-plugin.js
+++ optimizer-resolve-css-urls-plugin.js
@@ -1,8 +1,43 @@
 var cssParser = require('raptor-css-parser');
 var raptorModulesUtil = require('raptor-modules/util');
+var resolver = require('raptor-modules/resolver');
 var nodePath = require('path');
+var REQUIRE_PREFIX = 'require:';
+
+function defaultUrlResolver(url, optimizerContext, callback) {
+    if (url.charAt(0) === '/' && url.charAt(1) !== '/') {
+        url = nodePath.join(raptorModulesUtil.getProjectRootDir(url), url);
+    } else if (url.startsWith(REQUIRE_PREFIX)) {
+        url = url.substring(REQUIRE_PREFIX.length);
+
+        var from;
+        if (optimizerContext.dependency) {
+            from = optimizerContext.dependency.getDir(optimizerContext);
+        } else {
+            from = raptorModulesUtil.getProjectRootDir(url);
+        }
+
+        var query;
+        var pos = url.indexOf('?');
+        if (pos !== -1) {
+            query = url.substring(pos + 1);
+            url = url = url.substring(0, pos);
+        }
+
+        url = resolver.serverResolveRequire(url, from);
+
+        if (query) {
+            url += '?' + query;
+        }
+    }
+
+    callback(null, url);
+}
 
 module.exports = function (pageOptimizer, pluginConfig) {
+
+    var urlResolver = pluginConfig.urlResolver || defaultUrlResolver;
+
     pageOptimizer.addTransform({
         contentType: 'css',
 
@@ -22,16 +57,18 @@ module.exports = function (pageOptimizer, pluginConfig) {
                     code,
                     // the replacer function
                     function(url, start, end, callback) {
-                        if (url.charAt(0) === '/' && url.charAt(1) !== '/') {
-                            url = nodePath.join(raptorModulesUtil.getProjectRootDir(url), url);
-                        }
-                        
-                        optimizer.optimizeResource(url, optimizerContext, function(err, optimizedResource) {
-                            if (err) {
+                        urlResolver(url, optimizerContext, function(err, url) {
+                            if (err || !url) {
                                 return callback(err);
                             }
 
-                            callback(null, optimizedResource && optimizedResource.url);
+                            optimizer.optimizeResource(url, optimizerContext, function(err, optimizedResource) {
+                                if (err) {
+                                    return callback(err);
+                                }
+
+                                callback(null, optimizedResource && optimizedResource.url);
+                            });
                         });
                     }, function(err, code) {
                         if (err) {
